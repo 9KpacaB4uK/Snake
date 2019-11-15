a74f6af06417cc0ab934415ddbc721fe9e155f51
9KpacaB4uK <sanja810@inbox.lv>
2019-11-15 16:41:59 +0200

    Bug fix

--- src/SnakeMain.java
+++ src/SnakeMain.java
@@ -6,9 +6,10 @@
 */
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
-
+import javax.swing.Timer;
 public class SnakeMain {
     static int width_Screen = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getWidth();
     static int height_Screen = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getHeight();
@@ -17,18 +18,40 @@ public class SnakeMain {
     static int block = 40;
     static int gridW[]=new int[width_app/block];
     static int gridH[]=new int[ height_app/block];
-    public static void main(String[] args){
-        gameScreen gS = new gameScreen("Snake");
-        new SnakeMain().go();
+    static int rec_x=gridW[gridW.length/2];//start position X
+    static int rec_y=gridH[gridH.length/2];//start position Y
+    public gameScreen gS = new gameScreen("Snake");
+    public moveSnake mS = new moveSnake();
+    public static void main (String[] args) {
+        new SnakeMain();
     }
+    public class moveSnake{
+        public int x=0,y=0;
 
+        public void up() {
+            y -= block;
+        }
+        public void down(){
+            y+=block;
+        }
+        public void left(){
+            x-=block;
+        }
+        public void right(){
+            x+=block;
+        }
+
+    }
     public static class DPanel extends JPanel {
+
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             Graphics2D g2d = (Graphics2D) g;
             g2d.setColor(Color.green);
             g2d.setStroke(new BasicStroke(1));
             Line2D line;
+
+            //Draw grid X and Y
             int step = 0;
             for(int i=0;i<gridW.length;i++){
                 gridW[i] = step;
@@ -43,33 +66,63 @@ public class SnakeMain {
                 line = new Line2D.Double(0,step,width_app,step);
                 g2d.draw(line);
             }
-            int rec_x=gridW[1-1];//start position X
-            int rec_y=gridH[1-1];//start position Y
+
+            //Draw Hero
             Rectangle2D rect = new Rectangle2D.Double(rec_x, rec_y, block, block);
             g2d.setColor(Color.red);
             g2d.fillRect((int)rect.getX(),(int)rect.getY(),block,block);
             g2d.draw(rect);
             setBackground(Color.black);
+
         }
     }
-    public static class gameScreen extends JFrame {
+    public class gameScreen extends JFrame {
         public gameScreen(String title) throws HeadlessException {
             super(title);
-
             DPanel dPanel = new DPanel();
-
             setSize(width_app,height_app);
             setBounds(width_Screen/2-width_app/2,height_Screen/2-height_app/2,width_app,height_app);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//close on X
-           // setResizable(false);
+            //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//close on X
+            // setResizable(false);
             setUndecorated(true);//without title bar
             getContentPane().add(dPanel);
             setVisible(true);
-        }
+            addKeyListener(new KeyListener() {
+                @Override
+                public void keyTyped(KeyEvent e) {
 
-    }
-    void hero(){
-    }
-    void go(){
+                }
+                @Override
+                public void keyPressed(KeyEvent e) {
+
+                }
+
+                @Override
+                public void keyReleased(KeyEvent e) {
+                    int key = e.getKeyChar();
+                    if (key == 119 || key == 87){// W
+                        mS.up();
+                    }
+                    if (key == 83 || key == 115){ // S
+                        mS.down();
+                    }
+                    if (key == 97 || key == 65){ // A
+                        mS.left();
+                    }
+                    if (key == 100 || key == 68){ // D
+                        mS.right();
+                    }
+                }
+            });
+            new Timer(500, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    rec_x = mS.x;
+                    rec_y = mS.y;
+                    System.out.println(rec_x +" "+rec_y);
+                    gS.repaint();
+                }
+            }).start();
+        }
     }
 }
